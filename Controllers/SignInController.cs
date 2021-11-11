9d36d36ef34675a0951f2d289c8ee92c7b77a191
navid-nm <navidmomtahen@gmail.com>
2021-11-11 12:37:36 +0000

    Removed unused ILogger objects

--- Controllers/SignInController.cs
+++ Controllers/SignInController.cs
@@ -7,13 +7,6 @@ namespace AuctionSystemPOC.Controllers
 {
     public class SignInController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public SignInController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
