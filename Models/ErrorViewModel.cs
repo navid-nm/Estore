fa19748dd9e19d46d0747ca2b52ea891168f9df0
navid-nm <navidmomtahen@gmail.com>
2021-11-11 12:18:53 +0000

    Removed all previous proof of concept files

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EcomProofOfConcept.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
