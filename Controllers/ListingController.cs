ed98211a0770dc69d07573f965f16cd9ff466287
navid-nm <navidmomtahen@gmail.com>
2021-11-17 13:20:50 +0000

    Fixed MySQL out of range exception in bid validation

--- Controllers/ListingController.cs
+++ Controllers/ListingController.cs
@@ -17,7 +17,26 @@ namespace AuctionSystemPOC.Controllers
             item = new Item();
         }
 
-        public IActionResult Index(string id, decimal amount)
+        public void SetListingViewData(long idl)
+        {
+            Tuple<string, string, List<decimal>, string, string, bool, List<Bid>> info = item.GetInfo(idl);
+            if (info != null)
+            {
+                item.IncrementViews(idl);
+                ViewData["ID"] = idl.ToString();
+                ViewData["Name"] = info.Item1;
+                ViewData["Description"] = info.Item2;
+                ViewData["StartingPrice"] = info.Item3[0];
+                ViewData["CurrentPrice"] = info.Item3[1];
+                ViewData["Condition"] = info.Item4;
+                ViewData["Username"] = info.Item5;
+                ViewData["Concluded"] = info.Item6;
+                ViewData["Bids"] = info.Item7;
+                ViewData["IgnoreControllerValidation"] = false;
+            }
+        }
+
+        public IActionResult Index(string id, Bid bid)
         {
             decimal curprice = 0;
             long idl = 0;
@@ -40,31 +59,24 @@ namespace AuctionSystemPOC.Controllers
 
                 ViewData["BidPlaceAttempt"] = true;
                 if (sessionname == null) ViewData["Error"] = "Sign in to place a bid.";
-                else if (amount <= curprice) ViewData["Error"] = "Your bid must be higher than the current bid.";
+                else if (bid.Amount <= curprice) ViewData["Error"] = "Your bid must be higher than the current bid.";
                 else if (sessionname == seller) ViewData["Error"] = "You cannot bid on your own item.";
                 else
                 {
-                    item.AddBid(new Bid { ID = idl, Username = sessionname, Amount = amount });
+                    if (ModelState.IsValid)
+                    {
+                        item.AddBid(new Bid { ID = idl, Username = sessionname, Amount = bid.Amount });
+                    }
+                    else
+                    {
+                        SetListingViewData(idl);
+                        ViewData["IgnoreControllerValidation"] = true;
+                        return View();
+                    }
                 }
             }
-            if (pass)
-            {
-                Tuple<string, string, List<decimal>, string, string, bool, List<Bid>> info = item.GetInfo(idl);
-                if (info != null)
-                {
-                    item.IncrementViews(idl);
-                    ViewData["ID"] = id;
-                    ViewData["Name"] = info.Item1;
-                    ViewData["Description"] = info.Item2;
-                    ViewData["StartingPrice"] = info.Item3[0];
-                    ViewData["CurrentPrice"] = info.Item3[1];
-                    ViewData["Condition"] = info.Item4;
-                    ViewData["Username"] = info.Item5;
-                    ViewData["Concluded"] = info.Item6;
-                    ViewData["Bids"] = info.Item7;
-                }
-            }
-            return View("Index");
+            if (pass) SetListingViewData(idl);
+            return View();
         }
 
         [HttpGet]
--- Models/Bid.cs
+++ Models/Bid.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AuctionSystemPOC.Models
 {
     public class Bid
     {
         public long ID { get; set; }
+
+        [Required]
+        [Range(0, 999999998.99, ErrorMessage = "Invalid price")]
         public decimal Amount { get; set; }
+        
         public DateTime DateMade { get; set; }
         public string Username { get; set; }
     }
