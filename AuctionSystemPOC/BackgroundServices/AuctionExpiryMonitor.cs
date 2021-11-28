5709b61480af7bec9b91e13661377c157024a071
navid-nm <navidmomtahen@gmail.com>
2021-11-28 11:51:07 +0000

    Removed unused using statement in background service

--- AuctionSystemPOC/BackgroundServices/AuctionExpiryMonitor.cs
+++ AuctionSystemPOC/BackgroundServices/AuctionExpiryMonitor.cs
@@ -3,7 +3,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using AuctionSystemPOC.DataAccessLayers;
-using System.Diagnostics;
 
 namespace AuctionSystemPOC.BackgroundServices
 {
