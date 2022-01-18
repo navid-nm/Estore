3c5ac90beb7e038c01335e1b6c64a4c8061ee5b2
navid-nm <navidmomtahen@gmail.com>
2022-01-18 12:36:21 +0000

    Added corresponding migrations for email length limit change

---
+++ Estore/Migrations/20220118123306_LimitEmailSize.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estore.Migrations
+{
+    public partial class LimitEmailSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(254)",
+                maxLength: 254,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(254)",
+                oldMaxLength: 254);
+        }
+    }
+}
