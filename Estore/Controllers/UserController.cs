3ba31e7a1f38a1be47102db7d9aaace4e1378202
navid-nm <navidmomtahen@gmail.com>
2022-02-18 00:14:28 +0000

    Added item list to seller profile view

--- Estore/Controllers/UserController.cs
+++ Estore/Controllers/UserController.cs
@@ -1,22 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Estore.Data;
+using Estore.Models;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Estore.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IWebHostEnvironment _env;
         private readonly EstoreDbContext _context;
 
-        public UserController(EstoreDbContext context)
+        public UserController(IWebHostEnvironment hostenv, EstoreDbContext context)
         {
+            _env = hostenv;
             _context = context;
         }
 
         [HttpGet]
         public IActionResult Index(string username)
         {
-            ViewBag.User = _context.Users.FirstOrDefault(u => u.Username == username);
+            User user = _context.Users.Include(u => u.Items)
+                                      .FirstOrDefault(u => u.Username == username);
+            List<Item> items = new List<Item>();
+            foreach (Item item in user.Items)
+            {
+                items.Add(new ItemData(_context, _env).GetItem(item.FindCode));
+            }
+            ViewBag.User = user;
+            ViewBag.Items = items;
             return View();
         }
     }
