13d4fbb07e9c76cce939e4135d105886cae37bce
navid-nm <navidmomtahen@gmail.com>
2022-02-18 11:00:39 +0000

    Added autofocus to search bar

--- Estore/wwwroot/js/site.js
+++ Estore/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿// ...
+﻿const node = document.getElementById("search")
+
+function go() {
+    location.href = '/search/' + node.value
+}
+
+node.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
+        go()
+    }
+})
