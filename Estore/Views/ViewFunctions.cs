943d3cd301c0ca4df3d2d310b14afacfc521cfcf
navid-nm <navidmomtahen@gmail.com>
2022-03-13 12:44:52 +0000

    Added view functions to enforce OAOO in views

---
+++ Estore/Views/ViewFunctions.cs
@@ -0,0 +1,35 @@
+﻿using Estore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Estore.Views
+{
+    public class ViewFunctions
+    {
+        public List<string> GetImageUrls(Item item)
+        {
+            if (item != null)
+            {
+                if (item.ImageUrls != null)
+                {
+                    List<string> urls = new List<string>();
+                    foreach (string url in item.ImageUrls)
+                    {
+                        urls.Add("/" + url[url.IndexOf("img")..].Replace("\\", "/"));
+                    }
+                    return urls;
+                }
+            }
+            return new List<string> { "/img/generic/invertedplaceholder.png" };
+        }
+
+        public string NormaliseName(string name, int amount)
+        {
+            if (name.Length > amount)
+            {
+                name = name.Substring(0, Math.Min(amount, name.Length)) + "...";
+            }
+            return name;
+        }
+    }
+}
